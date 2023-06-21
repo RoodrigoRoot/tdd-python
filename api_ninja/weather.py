c0757e136a003c63bb45f7ca79b010374c6631cd
Rodrigo <rurcino@fondeadora.com>
2023-06-20 21:21:44 -0600

    refactoring to provider

--- api_ninja/weather.py
+++ api_ninja/weather.py
@@ -1,9 +1,10 @@
 import requests
 
-def send_request_temperature(city: str, country: str):
-    api_url = f'https://api.api-ninjas.com/v1/weather?city={city}&country={country}'
-    response = requests.get(api_url, headers={'X-Api-Key': 'Cp91WT2Iz2OtvNhFI8x/RQ==BNy1ErAMZmjeVWpq'})
-    if response.status_code == requests.codes.ok:
+def send_request_temperature(city: str, country: str) -> dict:
+    try:
+        api_url = f'https://api.api-ninjas.com/v1/weather?city={city}&country={country}'
+        response = requests.get(api_url, headers={'X-Api-Key': 'Cp91WT2Iz2OtvNhFI8x/RQ==BNy1ErAMZmjeVWpq'})
+        response.raise_for_status()
         return response.json()
-    else:
-        return []
+    except Exception:
+        return {}
--- tests/test_provider.py
+++ tests/test_provider.py
@@ -1,5 +1,7 @@
-from unittest import TestCase
-from unittest.mock import patch
+import requests
+
+from unittest import TestCase, skip
+from unittest.mock import patch, Mock
 
 from api_ninja.weather import send_request_temperature
 
@@ -8,27 +10,31 @@ from api_ninja.weather import send_request_temperature
 class ProviderAPINinjaTestCase(TestCase):
 
     def setUp(self) -> None:
-        self.get_requests_patch = patch('api_ninja.weather.requests.get')
+        self.get_requests_get_patch = patch('api_ninja.weather.requests')
 
-        self.get_requests_mock = self.get_requests_patch.start()
+        self.get_requests_get_mock = self.get_requests_get_patch.start()
+
+        self.expected_json_response = {"cloud_pct": 90, "temp": 36, "feels_like": 39, "humidity": 38,
+        "min_temp": 36, "max_temp": 36, "wind_speed": 3.5, "wind_degrees": 213, "sunrise": 1686916850, "sunset": 1686964075}
+
+        self.get_requests_get_mock.get.return_value.json.return_value = self.expected_json_response
 
         self.city = 'Acapulco'
         self.country = 'Mexico'
 
     def tearDown(self) -> None:
-        self.get_requests_patch.stop()
+        self.get_requests_get_mock.stop()
 
     def test_get_temperature_successfully(self):
-        self.get_requests_mock.json.return_value = {"cloud_pct": 90, "temp": 36, "feels_like": 39, "humidity": 38,
-                                                "min_temp": 36, "max_temp": 36, "wind_speed": 3.5, "wind_degrees": 213, "sunrise": 1686916850, "sunset": 1686964075}
-        self.get_requests_mock.status_code = 200
-        response = send_request_temperature(city=self.city, country=self.country)
 
+        response = send_request_temperature(city=self.city, country=self.country)
+        print(response)
         self.assertIsNotNone(response)
+        self.assertEqual(response, self.expected_json_response)
 
     def test_get_temperature_fail(self):
-        self.get_requests_mock.status_code = 500
+        self.get_requests_get_mock.get.return_value.raise_for_status.side_effect = Exception()
 
         response = send_request_temperature(city=self.city, country=self.country)
 
-        self.assertEqual(response, [])
+        self.assertEqual(response, {})
